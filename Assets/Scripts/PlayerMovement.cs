869839f9486ea90a259ce920bad1b51c3de0cfe0
Benjamin MARQUES <benjamin.marquesbalula@my-digital-school.org>
2024-01-08 14:52:45 +0100

    feat: Added player animation an flip

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,14 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 5f;
     public float speed = 10;
     private Animator anim;
+    private float dirX = 0f;
+    private SpriteRenderer sprite;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
 
@@ -26,17 +29,24 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
         if (dirX > 0f)
         {
             anim.SetBool("running", true);
+            sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
             anim.SetBool("running", true);
+            sprite.flipX = true;
         }
         else
         {
             anim.SetBool("running", false);
         }
-    } 
+    }
 }
