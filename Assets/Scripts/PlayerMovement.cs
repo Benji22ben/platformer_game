d6440731c980419699129fd4bc2704fd1b4aa7a5
Benjamin MARQUES <benjamin.marquesbalula@my-digital-school.org>
2024-01-08 14:38:09 +0100

    feat: Added running animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     public float jumpForce = 5f;
     public float speed = 10;
+    private Animator anim;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,5 +25,18 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump")) {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
-    }
+
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
+    } 
 }
